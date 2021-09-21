bcb4ae9cddbc4045243cc800beba519dac460380
MI <hello@superiss.com>
2021-09-21 09:32:14 +0100

    code clean

--- cmd/screen/main.go
+++ cmd/screen/main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"os"
 
+	"github.com/twiny/screenshot/cmd/screen/api"
+	"github.com/twiny/screenshot/internal/db"
+	"github.com/twiny/screenshot/pkg/logger"
+	"github.com/twiny/screenshot/pkg/rate"
 	"github.com/urfave/cli/v2"
 )
 
@@ -44,4 +48,38 @@ func startServerCmd() *cli.Command {
 			return startServer(c.String("config"))
 		},
 	}
+}
+
+// Start
+func startServer(path string) error {
+	// config
+	config, err := api.YAMLConfig(path)
+	if err != nil {
+		return err
+	}
+
+	// store
+	store, err := db.NewStore(config.StorePath)
+	if err != nil {
+		return err
+	}
+
+	// limiter
+	limiter := rate.NewLimiter(config.Rate, config.Bursts)
+
+	// logger
+	logger, err := logger.NewLogger(config.LogPath)
+	if err != nil {
+		return err
+	}
+
+	// new app
+	app := api.NewApp(config, store, limiter, logger)
+	defer app.Close()
+
+	// clean store & limiter
+	app.Sync()
+
+	// start http server
+	return api.NewHTTPServer(app).ListenAndServe(":" + config.Port)
 }
